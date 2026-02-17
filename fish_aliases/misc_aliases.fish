31378a0d0911e6b28a2ed0535a3072601562bede
Florian Schneider <schneider@roboception.de>
2026-02-17 12:44:37 +0100

    Fix srcvenv: convert complex alias to function

--- fish_aliases/misc_aliases.fish
+++ fish_aliases/misc_aliases.fish
@@ -1,2 +1,11 @@
-alias srcvenv='if test -d venv; . venv/bin/activate.fish; else if test -d .venv; . .venv/bin/activate.fish; else; echo "No venv or .venv found."; end'
+function srcvenv -d "Activate local virtual environment"
+    if test -d venv
+        source venv/bin/activate.fish
+    else if test -d .venv
+        source .venv/bin/activate.fish
+    else
+        echo "No venv or .venv found."
+    end
+end
+
 alias robotlaptopssh="ssh florian@10.0.1.51"
